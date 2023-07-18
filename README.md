eb2b7ce19f04aa26cdd6a55d592e859be43714b3
Mick Wollman <mick@bit-bar.org>
2023-07-18 10:18:24 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todoist Stack
+
+This is a tool that integrates with Todoist to help you to maintain a mental stack.
---
+++ todoist_stack.py
@@ -0,0 +1,6 @@
+
+def Usage():
+    print("Usage: python todoist_stack.py <pop|push [task name]>")
+
+if __name__ == "__main__":
+    Usage()
